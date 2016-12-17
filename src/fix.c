fe9b1d359097dacf4229f731774146f71865b3a4
Sebastian Freundt <freundt@ga-group.nl>
2016-12-17 08:33:01 +0000

    minor, import tools for fix, tls and hashing

---
+++ src/fix.c
@@ -0,0 +1,191 @@
+#if defined HAVE_CONFIG_H
+# include "config.h"
+#endif	/* HAVE_CONFIG_H */
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <sys/time.h>
+#include "fix.h"
+#include "nifty.h"
+
+#define CLCK_STR	"YYYYMMDD-HH:MM:SS.000"
+#define CLCK_FMT	"%Y%m%d-%H:%M:%S.000"
+
+static unsigned int seq = 1U;
+static const char *scomp_id;
+static size_t nscomp_id;
+static const char *tcomp_id;
+static size_t ntcomp_id;
+
+static char clck[] = CLCK_STR;
+static time_t metr;
+
+
+static uint8_t
+fix_chksum(const char *str, size_t len)
+{
+        unsigned int res = 0;
+        for (const char *p = str, *ep = str + len; p < ep; res += *p++);
+        return (uint8_t)(res & 0xff);
+}
+
+
+/* public API */
+void
+fix_reset(void)
+{
+	seq = 1U;
+	return;
+}
+
+size_t
+fix_render_tm(char *restrict buf, size_t UNUSED(bsz))
+{
+	time_t now = time(NULL);
+
+	if (UNLIKELY(now > metr)) {
+		struct tm *tm;
+		tm = gmtime(&now);
+		strftime(clck, sizeof(clck), CLCK_FMT, tm);
+	}
+	memcpy(buf, clck, strlenof(clck));
+	return strlenof(clck);
+}
+
+void
+fix_set_scomp_id(const char *sc, size_t sz)
+{
+	scomp_id = sc;
+	nscomp_id = sz;
+	return;
+}
+
+void
+fix_set_tcomp_id(const char *tc, size_t tz)
+{
+	tcomp_id = tc;
+	ntcomp_id = tz;
+	return;
+}
+
+size_t
+fix_render(char *restrict buf, size_t bsz, fix_msg_t msg)
+{
+	static const char fhdr[] = "8=FIX.4.4" SOH "9=XXX" SOH;
+	size_t len = 0U;
+
+	memcpy(buf, fhdr, len = strlenof(fhdr));
+
+	/* print message type */
+	buf[len++] = '3', buf[len++] = '5', buf[len++] = '=';
+	buf[len++] = msg.typ[0U];
+	if (LIKELY(!(buf[len++] = msg.typ[1U]))) {
+		len--;
+	}
+	buf[len++] = *SOH;
+
+	/* print sequence number */
+	buf[len++] = '3', buf[len++] = '4', buf[len++] = '=';
+	with (size_t prev = len) {
+		for (unsigned int s = seq++; s;
+		     buf[len++] = (char)((s % 10U) ^ '0'), s /= 10U);
+		for (size_t i = 0U; i < (len - prev) / 2U; i++) {
+			char tmp = buf[prev + i];
+			buf[prev + i] = buf[len - (i + 1U)];
+			buf[len - (i + 1U)] = tmp;
+		}
+	}
+	buf[len++] = *SOH;
+
+	/* print our comp id */
+	if (LIKELY(nscomp_id)) {
+		buf[len++] = '4', buf[len++] = '9', buf[len++] = '=';
+		memcpy(buf + len, scomp_id, nscomp_id);
+		len += nscomp_id;
+		buf[len++] = *SOH;
+	}
+
+	/* print time */
+	buf[len++] = '5', buf[len++] = '2', buf[len++] = '=';
+	len += fix_render_tm(buf + len, bsz - len);
+	buf[len++] = *SOH;
+
+	/* print target comp id */
+	buf[len++] = '5', buf[len++] = '6', buf[len++] = '=';
+	memcpy(buf + len, tcomp_id, ntcomp_id);
+	len += ntcomp_id;
+	buf[len++] = *SOH;
+
+	/* copy user message */
+	for (size_t i = 0U; i < msg.len; i++) {
+		buf[len++] = (char)(msg.msg[i] ? msg.msg[i] : *SOH);
+	}
+
+	with (size_t msz = len - strlenof(fhdr)) {
+		buf[strlenof(fhdr) - 2U] = (char)((msz % 10U) ^ '0');
+		msz /= 10U;
+		buf[strlenof(fhdr) - 3U] = (char)((msz % 10U) ^ '0');
+		msz /= 10U;
+		buf[strlenof(fhdr) - 4U] = (char)((msz % 10U) ^ '0');
+	}
+
+	/* print chksum */
+	with (uint8_t ck = fix_chksum(buf, len)) {
+		buf[len++] = '1', buf[len++] = '0', buf[len++] = '=';
+		buf[len + 2] = (char)((ck % 10U) ^ '0');
+		ck /= 10U;
+		buf[len + 1] = (char)((ck % 10U) ^ '0');
+		ck /= 10U;
+		buf[len + 0] = (char)((ck % 10U) ^ '0');
+		len += 3U;
+		buf[len++] = *SOH;
+	}
+
+	buf[len + 0U] = '\n';
+	buf[len + 1U] = '\0';
+	return len;
+}
+
+fix_msg_t
+fix_parse(char *restrict buf, size_t bsz)
+{
+	static const char fhdr[] = "8=FIX.4.4" SOH "9=";
+	fix_msg_t res;
+	size_t j;
+
+	if (UNLIKELY(bsz < strlenof(fhdr))) {
+		goto buggered;
+	} else if (UNLIKELY(memcmp(buf, fhdr, strlenof(fhdr)))) {
+		/* we want FIX.4.4 */
+		goto buggered;
+	}
+	/* look for that 35 tag (msg type),
+	 * we only scan messages whose lengths are 3 digits max */
+	if (j = sizeof(fhdr),
+	    buf[j++] != *SOH &&
+	    buf[j++] != *SOH &&
+	    buf[j++] != *SOH) {
+		goto buggered;
+	} else if (buf[j++] != '3' || buf[j++] != '5' || buf[j++] != '=') {
+		goto buggered;
+	}
+	/* now then, snarf message type */
+	res.typ[0U] = buf[j++];
+	res.typ[1U] = (uint8_t)(buf[j] == *SOH ? '\0' : buf[j++]);
+	res.typ[2U] = '\0', res.typ[3U] = '\0';
+
+	/* keep leading and trailing SOH */
+	res.msg = buf + j;
+	res.len = bsz - j;
+
+	/* NUL'ify instead of SOH */
+	for (size_t i = 0U; i < bsz; i++) {
+		buf[i] = (char)(buf[i] != *SOH ? buf[i] : *NUL);
+	}
+	return res;
+
+buggered:
+	return (fix_msg_t){};
+}
+
+/* fix.c ends here */
---
+++ src/tls.c
@@ -0,0 +1,144 @@
+#if defined HAVE_CONFIG_H
+# include "config.h"
+#endif	/* HAVE_CONFIG_H */
+#include <unistd.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <fcntl.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <errno.h>
+#include <openssl/hmac.h>
+#include "tls.h"
+#include "nifty.h"
+
+static SSL_CTX *sslctx;
+
+
+static __attribute__((format(printf, 1, 2))) void
+serror(const char *fmt, ...)
+{
+	va_list vap;
+	va_start(vap, fmt);
+	vfprintf(stderr, fmt, vap);
+	va_end(vap);
+	if (errno) {
+		fputc(':', stderr);
+		fputc(' ', stderr);
+		fputs(strerror(errno), stderr);
+	}
+	fputc('\n', stderr);
+	return;
+}
+
+static int
+put_sockaddr(struct sockaddr_in *sa, const char *name, uint16_t port)
+{
+	struct addrinfo hints;
+	struct addrinfo *res;
+
+	memset(sa, 0, sizeof(*sa));
+	memset(&hints, 0, sizeof(hints));
+
+	hints.ai_family = AF_INET;
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_flags = AI_PASSIVE;
+	if (getaddrinfo(name, NULL, &hints, &res)) {
+		return -1;
+	}
+	*sa = *(struct sockaddr_in*)res->ai_addr;
+	sa->sin_port = htons(port);
+	freeaddrinfo(res);
+	return 0;
+}
+
+
+/* public api */
+ssl_ctx_t
+open_tls(const char *host, short unsigned int port)
+{
+	struct sockaddr_in sa;
+	ssl_ctx_t this;
+	int rc;
+	int s;
+
+	if (UNLIKELY(put_sockaddr(&sa, host, port) < 0)) {
+		return NULL;
+	}
+	if (UNLIKELY((s = socket(PF_INET, SOCK_STREAM, IPPROTO_IP)) < 0)) {
+		return NULL;
+	}
+	if (UNLIKELY(connect(s, (void*)&sa, sizeof(sa)) < 0)) {
+		goto clo;
+	}
+
+	(void)SSL_library_init();
+	if (UNLIKELY(sslctx == NULL &&
+		     (sslctx = SSL_CTX_new(SSLv23_client_method())) == NULL)) {
+		goto clo;
+	} else if (UNLIKELY((this = SSL_new(sslctx)) == NULL)) {
+		goto clo;
+	}
+	SSL_set_fd(this, s);
+	if (UNLIKELY((rc = SSL_connect(this)) < 1)) {
+		serror("\
+Error: SSL connection failed, code %d", SSL_get_error(this, rc));
+		goto clo;
+	}
+	if (UNLIKELY((rc = SSL_do_handshake(this)) < 1)) {
+		serror("\
+Error: SSL handshake failed, code %d", SSL_get_error(this, rc));
+		goto clo;
+	}
+	return this;
+
+clo:
+	close(s);
+	return NULL;
+}
+
+int
+close_tls(ssl_ctx_t c)
+{
+	if (LIKELY(c != NULL)) {
+		int s = tls_fd(c);
+
+		SSL_shutdown(c);
+		SSL_free(c);
+
+		if (LIKELY(s >= 0)) {
+			close(s);
+		}
+	}
+	return 0;
+}
+
+size_t hmac(
+	char *restrict buf, size_t bsz,
+	const char *msg, size_t len,
+	const char *key, size_t ksz)
+{
+	unsigned int hlen = bsz;
+	unsigned char *sig = HMAC(
+		EVP_sha256(), key, ksz,
+		(const unsigned char*)msg, len,
+		NULL, &hlen);
+
+	/* hexl him */
+	for (size_t i = 0U; i < hlen * 2U; i += 2U) {
+		buf[i + 0U] = (char)((unsigned char)(sig[i / 2U] >> 4U) & 0xfU);
+		buf[i + 1U] = (char)((unsigned char)(sig[i / 2U] >> 0U) & 0xfU);
+	}
+	for (size_t i = 0U; i < hlen * 2U; i++) {
+		if ((unsigned char)buf[i] < 10U) {
+			buf[i] ^= '0';
+		} else {
+			buf[i] += 'W';
+		}
+	}
+	return hlen;
+}
+
+/* tls.c ends here */
