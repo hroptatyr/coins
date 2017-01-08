85e93b0ac7d759bc1750160a280ba3455066b24b
Sebastian Freundt <freundt@ga-group.nl>
2017-01-08 08:46:23 +0000

    chore, also unloop coin(1)

--- src/coin.c
+++ src/coin.c
@@ -305,15 +305,9 @@ ws_cb(EV_P_ ev_io *w, int UNUSED(revents))
 
 unroll:
 	/* connection reset */
-	loghim("restart in 3", 12);
-	sleep(1);
-	loghim("restart in 2", 12);
-	sleep(1);
-	loghim("restart in 1", 12);
-	sleep(1);
-	loghim("restart", 7);
 	ctx->nothing = 0;
 	ctx->st = COIN_ST_RECONN;
+	ev_unloop(EV_A_ EVUNLOOP_ALL);
 	return;
 }
 
