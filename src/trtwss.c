4c12869d22a2fe7ba0ffcde0f8fd2754ea59b8bf
Sebastian Freundt <freundt@ga-group.nl>
2017-01-12 13:29:35 +0000

    fix, PEBKAC, bring back unquoting of data cells for trtwss(1)

--- src/trtwss.c
+++ src/trtwss.c
@@ -150,6 +150,63 @@ close_sock(int fd)
 	return close(fd);
 }
 
+static char*
+xmemmem(const char *hay, const size_t hayz, const char *ndl, const size_t ndlz)
+{
+	const char *const eoh = hay + hayz;
+	const char *const eon = ndl + ndlz;
+	const char *hp;
+	const char *np;
+	const char *cand;
+	unsigned int hsum;
+	unsigned int nsum;
+	unsigned int eqp;
+
+	/* trivial checks first
+         * a 0-sized needle is defined to be found anywhere in haystack
+         * then run strchr() to find a candidate in HAYSTACK (i.e. a portion
+         * that happens to begin with *NEEDLE) */
+	if (ndlz == 0UL) {
+		return deconst(hay);
+	} else if ((hay = memchr(hay, *ndl, hayz)) == NULL) {
+		/* trivial */
+		return NULL;
+	}
+
+	/* First characters of haystack and needle are the same now. Both are
+	 * guaranteed to be at least one character long.  Now computes the sum
+	 * of characters values of needle together with the sum of the first
+	 * needle_len characters of haystack. */
+	for (hp = hay + 1U, np = ndl + 1U, hsum = *hay, nsum = *hay, eqp = 1U;
+	     hp < eoh && np < eon;
+	     hsum ^= *hp, nsum ^= *np, eqp &= *hp == *np, hp++, np++);
+
+	/* HP now references the (NZ + 1)-th character. */
+	if (np < eon) {
+		/* haystack is smaller than needle, :O */
+		return NULL;
+	} else if (eqp) {
+		/* found a match */
+		return deconst(hay);
+	}
+
+	/* now loop through the rest of haystack,
+	 * updating the sum iteratively */
+	for (cand = hay; hp < eoh; hp++) {
+		hsum ^= *cand++;
+		hsum ^= *hp;
+
+		/* Since the sum of the characters is already known to be
+		 * equal at that point, it is enough to check just NZ - 1
+		 * characters for equality,
+		 * also CAND is by design < HP, so no need for range checks */
+		if (hsum == nsum && memcmp(cand, ndl, ndlz - 1U) == 0) {
+			return deconst(cand);
+		}
+	}
+	return NULL;
+}
+
 
 static inline size_t
 memncpy(char *restrict tgt, const char *src, size_t zrc)
@@ -163,6 +220,75 @@ memnmove(char *tgt, const char *src, size_t zrc)
 {
 	memmove(tgt, src, zrc);
 	return zrc;
+}
+
+static char*
+next_data(const char *buf, size_t bsz)
+{
+       static const char tok[] = "\"data\"";
+       const char *const eob = buf + bsz;
+       const char *bp = buf;
+
+       for (size_t bz;
+            (bp = xmemmem(bp, eob - bp, tok, strlenof(tok)));
+            bp += bz) {
+               /* by default skip strlenof(tok) */
+               bz = strlenof(tok);
+
+               /* overread whitespace */
+               for (; (unsigned char)bp[bz] <= ' '; bz++);
+               /* must be : */
+               if (bp[bz++] != ':') {
+                       continue;
+               }
+               /* overread whitespace again */
+               for (; (unsigned char)bp[bz] <= ' '; bz++);
+               /* must be " */
+               if (bp[bz++] != '"') {
+                       continue;
+               }
+               return deconst(bp + bz - 1U);
+       }
+       return NULL;
+}
+
+static size_t
+massage(char *restrict buf, size_t bsz)
+{
+	const char *const eob = buf + bsz;
+	const char *x;
+	size_t t;
+
+	/* get first extent */
+	if ((x = next_data(buf, bsz)) == NULL) {
+		return bsz;
+	}
+	/* initialise target pointer */
+	t = x - buf;
+
+	for (const char *y;; x = y) {
+		/* shrink buffer, de-escaping things */
+		for (;; t++) {
+			if ((buf[t] = *++x) == '\\') {
+				/* copy next one */
+				buf[t] = *++x;
+			} else if (buf[t] == '"') {
+				/* we'done */
+				x++;
+				break;
+			}
+		}
+		/* try and find the next occurrence */
+		if ((y = next_data(x, eob - x)) == NULL) {
+			/* no more hits */
+			break;
+		}
+		/* and copy in between X and Y */
+		t += memnmove(buf + t, x, y - x);
+	}
+	/* and copy in between X and Y */
+	t += memnmove(buf + t, x, eob - x);
+	return t;
 }
 
 static int
@@ -194,10 +320,12 @@ logwss(const char *buf, size_t len)
 	for (const char *eol, *const ep = buf + len;
 	     lp < ep && (eol = memchr(lp, '\n', ep - lp));
 	     lp = eol + 1U) {
-		memmove(gbuf + prfz, lp, eol - lp);
-		gbuf[prfz + (eol - lp)] = '\n';
-		write(logfd, gbuf, prfz + (eol - lp) + 1U);
-		fwrite(gbuf, 1, prfz + (eol - lp) + 1U, stderr);
+		size_t z;
+		z = memnmove(gbuf + prfz, lp, eol - lp);
+		gbuf[prfz + z++] = '\n';
+		z = massage(gbuf + prfz, z);
+		write(logfd, gbuf, prfz + z);
+		fwrite(gbuf, 1, prfz + z, stderr);
 	}
 	return lp - buf;
 }
