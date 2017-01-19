78cd1a888834add73b599154912cab838981d5c5
Sebastian Freundt <freundt@ga-group.nl>
2017-01-19 13:13:13 +0000

    minor, migrate huobi(1) and spacebtc(1) to sio protocol identifier

--- src/huobi.c
+++ src/huobi.c
@@ -6,20 +6,11 @@
 #include <string.h>
 #include <unistd.h>
 #include <stdint.h>
-#include <fcntl.h>
-#include <sys/time.h>
-#include <time.h>
-#include <sys/mman.h>
-#include <netinet/in.h>
-#include <sys/socket.h>
-#include <arpa/inet.h>
-#include <netdb.h>
 #include <errno.h>
-#include <curl/curl.h>
 #include "wssnarf.h"
 #include "nifty.h"
 
-#define API_URL		"ws://hq.huobi.com/socket.io/1/"
+#define API_URL		"sio1://hq.huobi.com"
 
 
 int
@@ -58,19 +49,6 @@ join_coin(ws_t ws)
 }
 
 
-static ssize_t
-sid_cb(const void *data, const size_t nz, const size_t nm, void *clo)
-{
-	/* find colon */
-	size_t z;
-
-	for (z = 0U; z < nz * nm && ((const char*)data)[z] != ':'; z++);
-	memcpy(clo, "websocket/", 10U);
-	memcpy((char*)clo + 10U, data, z);
-	return nz * nm;
-}
-
-
 #include "huobi.yucc"
 
 int
@@ -83,22 +61,6 @@ main(int argc, char *argv[])
 
 	if (yuck_parse(argi, argc, argv) < 0) {
 		return EXIT_FAILURE;
-	}
-
-	/* obtain a session handle first *sigh* */
-	curl_global_init(CURL_GLOBAL_ALL);
-	with (CURL *hndl = curl_easy_init()) {
-		curl_easy_setopt(hndl, CURLOPT_VERBOSE, 1L);
-		curl_easy_setopt(hndl, CURLOPT_NOPROGRESS, 1L);
-		curl_easy_setopt(hndl, CURLOPT_WRITEFUNCTION, sid_cb);
-		curl_easy_setopt(hndl, CURLOPT_WRITEDATA,
-				 api_url + strlenof(API_URL));
-		curl_easy_setopt(hndl, CURLOPT_USERAGENT,
-				 "Dark Secret Ninja/1.0");
-
-		curl_easy_setopt(hndl, CURLOPT_URL, api_url + 5U/*ws:...*/);
-		curl_easy_perform(hndl);
-		curl_easy_cleanup(hndl);
 	}
 
 	/* obtain a loop */
